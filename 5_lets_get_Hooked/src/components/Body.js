3fab5b45f91bc76fe3f5d02f34761f75f226f542
alok kumar <alok60541@gmail.com>
2024-04-11 22:24:21 +0530

    ep-5 (ep-4 Restructured) import/export

---
+++ 5_lets_get_Hooked/src/components/Body.js
@@ -0,0 +1,21 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+// ** Body (main container) component **
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                
+                {
+                    resList.map((restaurant) => (
+                        <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+                    ))    
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Body;
