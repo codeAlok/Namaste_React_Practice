0136545427d28669c96509380f277c86b0b94b6f
alok kumar <alok60541@gmail.com>
2024-04-26 21:56:49 +0530

    ep-7 Basic routing(react-router), browserRouting

--- 7_Finding_the_path/src/App.js
+++ 7_Finding_the_path/src/App.js
@@ -2,17 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header"; // /Header or /Header.js (both work fine)
 import Body from "./components/Body.js";
-
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
+import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
  
 // ****** Main App Component (starting point) ******
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+
+            {/* Here <outlet /> will automatically replaced by component inside children of applayout in createBrowserRouter based on path searched */}
+            <Outlet />
+            
         </div>
     );
 };
 
+// **** Creating Routing configuration *****
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+        ],
+        errorElement: <Error />, 
+    },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />); 
+// root.render(<AppLayout />); 
+
+// <RouterProvider />  (a component by react-router-dom)
+root.render( <RouterProvider router={appRouter} /> );
--- 7_Finding_the_path/src/components/Header.js
+++ 7_Finding_the_path/src/components/Header.js
@@ -1,5 +1,6 @@
 import { LOGO_URL } from "../utils/constants";
 import {useState} from "react";
+import { Link } from "react-router-dom"; // A component used instead of anchor tag
 
 const Header = () => {
     const [btnName, setBtnName] = useState("login");
@@ -11,10 +12,12 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
+                    {/* <link></link> this helps in routing to diffrent pages without reloading whole page */}
+                    {/* In html output it behave/show as an anchor tag */}
+                    <li> <Link to="/">Home</Link> </li>
+                    <li> <Link to="/about">About Us</Link> </li>
+                    <li> <Link to="/contact">Contact Us</Link> </li>
+                    <li> <Link to="/cart">Cart</Link> </li>
                     <button 
                         className="login"
                         onClick = {() => {
