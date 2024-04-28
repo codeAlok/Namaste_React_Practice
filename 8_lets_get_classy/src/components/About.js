07ff2343938c4702533ae76d4b55d0c7dc7bc062
alok kumar <alok60541@gmail.com>
2024-04-28 16:47:01 +0530

    class component (create + update stateVariable) compared to fn component

--- 8_lets_get_classy/src/components/About.js
+++ 8_lets_get_classy/src/components/About.js
@@ -1,8 +1,15 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
         <div>
             <h1>About Us page</h1>
             <h4>Know more about Me</h4>
+
+            <User name={"Alok for function"} location={"India"}/>
+            
+            <UserClass name={"Alok for class"} location={"India"}/>
         </div>
     );
 };
---
+++ 8_lets_get_classy/src/components/UserClass.js
@@ -0,0 +1,52 @@
+// **** class based components for User ****
+import React from "react"; 
+
+// Normal Class + React.Component (makes it class based component)
+class UserClass extends React.Component {
+    
+    // *** constructor() is called , each time when an instace of the class created / class get rendered ***
+    constructor(props) {
+        super(props);  // must use super(props), to access props anywhere inside class
+
+        //** to create state variable in class component**
+        // ** Here this.state={} is a large object which can hold lots of state variable
+        this.state = {
+            count: 0,
+            count2: 12,
+        }
+
+    }
+
+    // render() method returning JSX
+    render() {
+        // Destructuring
+        const {name, location} = this.props;
+        const {count, count2} = this.state;
+
+        return (
+            <div className="user-card">
+                <h1>Inside Class Component</h1>
+
+                <p>Count: {count}</p>
+                <button
+                    onClick={ () => {
+                        // Never update state variable directly like this
+
+                        // must use this.setState({ }) to update state variable in class component (takes an object)
+                        this.setState({
+                            count: this.state.count + 1,
+                        });
+                    }}
+                >
+                    Count Increase
+                </button>
+
+                <h2>Name: {name}</h2>          
+                <h3>Location: {location}</h3>
+                <h4>Contact: @codealok </h4>
+            </div>
+        );
+    }
+}
+
+export default UserClass;
