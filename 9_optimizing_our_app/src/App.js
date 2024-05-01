3dff8e66f5cd24721cc93da79077d4b0eb172f75
alok kumar <alok60541@gmail.com>
2024-05-01 08:57:29 +0530

    ep-9 initial (ep-8 final code)

---
+++ 9_optimizing_our_app/src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
+import Header from "./components/Header"; // /Header or /Header.js (both work fine)
+import Body from "./components/Body.js";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
+import RestaurantMenu from "./components/RestaurantMenu.js";
+ 
+// ****** Main App Component (starting point) ******
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+
+            {/* Here <outlet /> will automatically replaced by component inside children of applayout in createBrowserRouter based on path searched */}
+            <Outlet />
+            
+        </div>
+    );
+};
+
+// **** Creating Routing configuration *****
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+            {
+                path: "/restaurant/:resId",
+                element: <RestaurantMenu />,
+            },
+        ],
+        errorElement: <Error />, 
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<AppLayout />); 
+
+// <RouterProvider />  (a component by react-router-dom)
+root.render( <RouterProvider router={appRouter} /> );
