427835a101713da855d3e95ed0ff72c0aac042d3
alok kumar <alok60541@gmail.com>
2024-05-02 15:58:08 +0530

    ep-10 initial (ep-9 last)

---
+++ 10_jo_dikhta_hai/src/components/Header.js
@@ -0,0 +1,41 @@
+import { LOGO_URL } from "../utils/constants";
+import {useState} from "react";
+import { Link } from "react-router-dom"; // A component used instead of anchor tag
+import useOnlineStatus from "../utils/useOnlineStatus";
+
+const Header = () => {
+    const [btnName, setBtnName] = useState("login");
+
+    const onlineStatus = useOnlineStatus();
+
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={LOGO_URL} alt="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
+
+                    {/* <link></link> this helps in routing to diffrent pages without reloading whole page */}
+                    {/* In html output it behave/show as an anchor tag */}
+                    <li> <Link to="/">Home</Link> </li>
+                    <li> <Link to="/about">About Us</Link> </li>
+                    <li> <Link to="/contact">Contact Us</Link> </li>
+                    <li> <Link to="/grocery">Grocery</Link> </li>
+                    <li> <Link to="/cart">Cart</Link> </li>
+                    <button 
+                        className="login"
+                        onClick = {() => {
+                            btnName === "login" ? setBtnName("logout") : setBtnName("login");
+                        }}
+                    >
+                        {btnName}
+                    </button>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
